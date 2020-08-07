4c01801c30320e0dce69c5c055bea2937c6b07e9
ajaypanthagani <ajaypanthagani321@gmail.com>
2020-08-07 19:18:07 +0530

    deleted component imports from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 /*components imports*/
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ExploreComponent } from './explore/explore.component';
-import { ProfileComponent } from './profile/profile.component';
-import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ExploreComponent,
-    ProfileComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
